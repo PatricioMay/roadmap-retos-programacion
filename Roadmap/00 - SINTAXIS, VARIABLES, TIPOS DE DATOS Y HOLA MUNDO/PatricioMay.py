3e35e932356d9db3bddcaeda4ff9e6b0fe0b9006
PatricioMay <juanmayok7@gmail.com>
2025-04-15 21:51:28 -0300

    Create PatricioMay.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/PatricioMay.py
@@ -0,0 +1,29 @@
+# https://www.python.org
+
+# Comentario en linea
+
+"""
+comentario 
+en varias
+lineas
+"""
+
+my_variable = "Mi variable"
+my_variable = "nuevo valor de mi variable"
+
+MY-CONSTANT = "Mi constante"
+MY_CONSTANT = "books"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "variable 1"
+my_other_string = 'variable 2'
+
+print ("Hola, Phyton")
+
+print (type(my_int))
+print (type(my_float))
+print (type(my_bool))
+print (type(my_string))
